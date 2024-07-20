baa199ae2450127f607e8b09f8394e941892ce06
manumn95 <manumnofficial.95@gmail.com>
2024-07-20 14:33:25 +0530

    Recreated the app and fixed the bugs

--- client/src/pages/CheckEmailPage.js
+++ client/src/pages/CheckEmailPage.js
@@ -30,9 +30,7 @@ const CheckEmailPage = () => {
     const URL = `${process.env.REACT_APP_BACKEND_URL}/api/email`;
 
     try {
-      const response = await axios.post(URL, data,{
-        withCredentials:true
-      });
+      const response = await axios.post(URL, data);
 
       toast.success(response.data.message);
 
